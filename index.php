5f3b64ae29b598bab099550912c04645af7378ba
Samiur-UTI <allpraisethelord@gmail.com>
2021-02-27 03:50:37 +0600

    DONE

--- index.php
+++ index.php
@@ -12,7 +12,25 @@
 				$msgClass = 'alert-danger';
 
 			} else {
-				echo 'Passed';
+				$toEmail = 'allpraisehtlord@gmail.com';
+				$subject = 'Contact Request from'. $name;
+				$body = '<h2>Contact Request</h2>
+						 <h4>Name</h4><p>'.$name.'</p>
+						 <h4>Email</h4><p>'.$email.'</p>
+						 <h4>Message</h4><p>'.$message.'</p> 
+						';
+				$headers = 'MIME-Version: 1.0' .'\r\n';
+				$headers .= "Content-Type:text/html;charset=UTF-8"."\r\n";
+				$headers .=  "From". $name. "<".$email.">"."\r\n";
+
+				if(mail($toEmail, $subject, $body)){
+					$msg = 'Your Email has been sent';
+					$msgClass = 'alert-success';
+				} else {
+					$msg = "Please fill out the fields properly";
+					$msgClass = "alert-danger";
+				}
+
 			}
 		} else {
 			$msg = 'Please Fill in all fields';
